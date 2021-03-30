c84b11ce4adba79693ffb525367cae1503a8ec57
JM0407 <80819481+JM0407@users.noreply.github.com>
2021-03-30 18:34:57 +0800

    added dictionary

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from flask_restful import Api, Resource
 app = Flask(__name__)                     #create a flask app
 api = Api(app)                            # wrap the app with an api
 
+names = { "JM":{"age":26,"gender":"male","birthday":"04/07/1995"},
+          "KATE":{"age":27,"gender":"female","birthday":"03/14/1994"}       #This section is to store information in the memory by using dictionary
+}
+
 class HelloWorld(Resource):               # This is to create a class that contains a resource
 
     """
@@ -16,10 +20,16 @@ class HelloWorld(Resource):               # This is to create a class that conta
 api.add_resource(HelloWorld, "/helloworld")          # registering the resource to the api , (class, "endpoint")
     """
 
+    """
 #THIS IS FOR PASSING ARGUMENT
     def get(self, name, age):                                                # GET request
         return {"name": name, "age": age}                                    # return message of the endpoint (key value pair for json format)
 api.add_resource(HelloWorld, "/helloworld/<string:name>/<int:age>")          # registering the resource to the api , (class, "endpoint"), argument <datatype:userinput>
+   """
+
+    def get(self, name):                                                # GET request
+        return names[name]                                  # return message of the endpoint (key value pair for json format)
+api.add_resource(HelloWorld, "/helloworld/<string:name>")          # registering the resource to the api , (class, "endpoint"), argument <datatype:userinput>
 
 
 if __name__ == "__main__":
