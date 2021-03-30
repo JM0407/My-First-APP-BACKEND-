b8d480f8cada83bd07f9ba988dbd156b7ff423e6
JM0407 <80819481+JM0407@users.noreply.github.com>
2021-03-30 17:21:27 +0800

    added get request

--- main.py
+++ main.py
@@ -4,5 +4,11 @@ from flask_restful import Api, Resource
 app = Flask(__name__)                     #create a flask app
 api = Api(app)                            # wrap the app with an api
 
+class HelloWorld(Resource):               # This is to create a class that contains a resource
+    def get(self):                        # Get request
+        return {"data": "hello world!"}           # return message of the endpoint
+
+api.add_resource(HelloWorld, "/helloworld")          # registering the resource to the api , (class, "endpoint")
+
 if __name__ == "__main__":
     app.run(debug=True)                   #run the app , use debug only on dev env
